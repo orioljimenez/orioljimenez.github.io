1eee3664ae913481d2f37ae8055fb821ff1dd29e
Oriol Jiménez <o.jimenez@resto-in.com>
2013-11-29 16:26:36 +0100

    Better email address obfuscation

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+
+$(function() {
+    var qFbvxclnQLTcJHW=["111","114","105","111","x6c","x40","x70","x68","112","x62","115","x64","x2e","x6e","x65","116"];
+    var nrfYhgZBLYPNnPn=[""];
+    var mUKTVDlXVTKAhsS=["111","x72","105","111","108","64","x70","x68","x70","98","115","100","x2e","x6e","101","116"];
+    for (i=0; i<mUKTVDlXVTKAhsS.length; i++) $('#IriWLVybTfiARCX').append('&#'+mUKTVDlXVTKAhsS[i]+';');
+
+    $('#IriWLVybTfiARCX').attr('href', '\u006D\u0061'+'ilto\u003A'+$('#IriWLVybTfiARCX').text());
+});
+
